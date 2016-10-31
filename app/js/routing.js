976cb2677ed8f93bd90715ccea46d72574b90de4
Andy Kulis <akulis@users.noreply.github.com>
2016-10-31 14:28:17 -0500

    Update routing.js

--- app/js/routing.js
+++ app/js/routing.js
@@ -45,7 +45,6 @@ four51.app.config(['$routeProvider', '$locationProvider', function($routeProvide
         when('/reports', { templateUrl: 'partials/reportsView.html', controller: 'ReportsCtrl' }).
         when('/report/:id', { templateUrl: 'partials/Reporting/reportView.html', controller: 'ReportCtrl' }).
         when('/contactus', { templateUrl: 'partials/Messages/contactus.html' }).
-        otherwise({redirectTo: '/catalog'});
         when('/templates', { templateUrl: 'partials/Messages/templates.html' }).
-        otherwise({redirectTo: '/templates'});
+        otherwise({redirectTo: '/catalog'});
 }]);
